480af22c1e554cf13455b960a2fe24950b62fc27
sahadan <sahadan@gmail.com>
2021-08-18 04:18:16 +0530

    First commit

---
+++ springboot-restapi-demo-2022v3/src/main/java/com/faith/app/SpringbootRestapiDemo2022v3Application.java
@@ -0,0 +1,14 @@
+package com.faith.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiDemo2022v3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiDemo2022v3Application.class, args);
+		System.out.println("Welcome !");
+	}
+
+}
---
+++ springboot-restapi-demo-2022v3/src/test/java/com/faith/app/SpringbootRestapiDemo2022v3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faith.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestapiDemo2022v3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
